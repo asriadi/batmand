aa9df208ee0dd8e5c78c0877105748f5316b669c
marek <marek@45894c77-fb22-0410-b583-ff6e7d5dbf6c>
2007-02-23 16:30:54 +0000

    updating perl debug script

--- trunk/batman-debug4.pl
+++ trunk/batman-debug4.pl
@@ -4,35 +4,46 @@ use strict;
 use utf8;
 
 
-my ( %myself_hash, %receive_hash, $last_orig, $last_neigh, $orig_interval, $total_seconds );
+my ( %myself_hash, %receive_hash, %forward_hash, $last_orig, $last_neigh, $last_seq, $last_ttl, $orig_interval, $total_seconds, %to_do_hash, %seq_hash );
 
 $orig_interval = 1000;
 
 
-if ( ! -e $ARGV[0] ) {
+if ( ( $ARGV[0] ne "-s" ) && ( $ARGV[0] ne "-p" ) && ( $ARGV[0] ne "-r" ) ) {
 
-	print "B.A.T.M.A.N log file could not be found: $ARGV[0]\n";
+	print "Usage: batman-debug4.pl option <file>\n";
+	print "\t-p    packet statistic\n";
+	print "\t-s    sequence number statistic\n";
+	print "\t-r    routing statistic (not implemented yet)\n";
+	exit;
+
+}
+
+if ( ! -e $ARGV[1] ) {
+
+	print "B.A.T.M.A.N log file could not be found: $ARGV[1]\n";
 	exit;
 
 }
 
 
-open(BATMANLOG, "< $ARGV[0]");
+open(BATMANLOG, "< $ARGV[1]");
 
-while (<BATMANLOG>) {
+while ( <BATMANLOG> ) {
 
-
-	if ( m/Received\ BATMAN\ packet\ from\ ([\d]+\.[\d]+\.[\d]+\.[\d]+).*?originator\ ([\d]+\.[\d]+\.[\d]+\.[\d]+)/ ) {
+	if ( m/Received\ BATMAN\ packet\ from\ ([\d]+\.[\d]+\.[\d]+\.[\d]+).*?originator\ ([\d]+\.[\d]+\.[\d]+\.[\d]+),\ seqno ([\d]+),\ TTL ([\d]+)/ ) {
 
 		$receive_hash{ $2 }{ $1 }{ "num_recv" }++;
 		$last_orig = $2;
 		$last_neigh = $1;
+		$last_seq = $3;
+		$last_ttl = $4;
 
 	} elsif ( m/Forwarding\ packet\ \(originator\ ([\d]+\.[\d]+\.[\d]+\.[\d]+)/ ) {
 
 		if ( $1 eq $last_orig ) {
 
-			$receive_hash{ $last_orig }{ $last_neigh }{ "num_forw" }++;
+# 			$receive_hash{ $last_orig }{ $last_neigh }{ "num_forw" }++;
 
 		} elsif ( $myself_hash{ $1 } ) {
 
@@ -40,37 +51,69 @@ while (<BATMANLOG>) {
 
 		} else {
 
-			print "Not equal: $_\n"
+			print "Not equal: $_ <> $1\n"
 
 		}
 
-	} elsif ( m/not\ my\ best\ neighbour/ ) {
+	} elsif ( m/Drop\ packet:/ ) {
 
-		$receive_hash{ $last_orig }{ $last_neigh }{ "not_best" }++;
+		$receive_hash{ $last_orig }{ $last_neigh }{ "num_drop" }++;
+
+		if ( m/incompatible\ batman\ version/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "version" }++;
+
+		} elsif ( m/received\ my\ own\ broadcast/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "own_broad" }++;
+
+		} elsif ( m/originator\ packet\ from\ myself/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "own_rebroad" }++;
+
+		} elsif ( m/originator\ packet\ with\ unidirectional\ flag/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "uni_flag" }++;
+
+		} elsif ( m/received\ via\ unidirectional\ link/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "uni_link" }++;
+
+		} elsif ( m/duplicate\ packet/ ) {
+
+			$receive_hash{ $last_orig }{ $last_neigh }{ "dup" }++;
+
+		}
+
+	} elsif ( m/Forward packet:/ ) {
+
+		$forward_hash{ $last_orig }{ $last_neigh }{ "num_forw" }++;
+
+		if ( m/rebroadcast\ neighbour\ packet\ with\ direct\ link\ flag/ ) {
+
+			$forward_hash{ $last_orig }{ $last_neigh }{ "direct_link" }++;
+
+		} elsif ( m/rebroadcast\ neighbour\ packet\ with\ direct\ link\ and\ unidirectional\ flag/ ) {
+
+			$forward_hash{ $last_orig }{ $last_neigh }{ "direct_uni" }++;
+
+		} elsif ( m/rebroadcast\ orginator\ packet/ ) {
+
+			$forward_hash{ $last_orig }{ $last_neigh }{ "rebroad" }++;
+
+		} elsif ( m/duplicate\ packet\ received\ via\ best\ neighbour\ with\ best\ ttl/ ) {
+
+			$forward_hash{ $last_orig }{ $last_neigh }{ "dup" }++;
+
+		}
+
+	} elsif ( m/update_originator/ ) {
+
+		push( @{ $seq_hash{ $last_orig }{ $last_neigh } }, "$last_seq [$last_ttl]" );
 
 	} elsif ( m/ttl\ exceeded/ ) {
 
 		$receive_hash{ $last_orig }{ $last_neigh }{ "ttl" }++;
-
-	} elsif ( m/Packet\ with\ unidirectional\ flag/ ) {
-
-		$receive_hash{ $last_orig }{ $last_neigh }{ "uni" }++;
-
-	} elsif ( m/received\ via\ bidirectional\ link/ ) {
-
-		$receive_hash{ $last_orig }{ $last_neigh }{ "bi" }++;
-
-	} elsif ( m/neighbour\ thinks\ connection\ is\ bidirectional\ -\ I\ disagree/ ) {
-
-		$receive_hash{ $last_orig }{ $last_neigh }{ "disagree" }++;
-
-	} elsif ( m/Duplicate\ packet/ ) {
-
-		$receive_hash{ $last_orig }{ $last_neigh }{ "dup" }++;
-
-	} elsif ( m/Incompatible\ batman\ version/ ) {
-
-		$receive_hash{ $last_orig }{ $last_neigh }{ "incom" }++;
 
 	} elsif ( m/Using\ interface\ (.*?)\ with\ address\ ([\d]+\.[\d]+\.[\d]+\.[\d]+)/ ) {
 
@@ -80,58 +123,107 @@ while (<BATMANLOG>) {
 
 		$orig_interval = $1;
 
+	} elsif ( m/\[.*\](.*)/ ) {
+
+		$to_do_hash{ $1 }++;
+
+	}
+
+}
+
+
+close( BATMANLOG );
+
+
+if ( $ARGV[0] eq "-p" ) {
+
+	print "\nSent:\n^^^^\n";
+
+	foreach my $my_ip ( keys %myself_hash ) {
+
+		$total_seconds = ( $myself_hash{ $my_ip }{ "sent" } * $orig_interval ) / 1000;
+		print " => $my_ip (" . $myself_hash{ $my_ip }{ "if" } . "): send " . $myself_hash{ $my_ip }{ "sent" } . " packets in $total_seconds seconds\n";
+
+	}
+
+
+	print "\n\nReceived:\n^^^^^^^^";
+
+	foreach my $orginator ( keys %receive_hash ) {
+
+		my $sum = 0;
+		my $string = "";
+
+		foreach my $neighbour ( keys %{ $receive_hash{ $orginator } } ) {
+
+			$sum += $receive_hash{ $orginator }{ $neighbour }{ "num_recv" };
+			$string .= " => $neighbour" . ( $myself_hash{ $neighbour } ? " (myself):\t" : ": \t\t" );
+			$string .= " recv = " . $receive_hash{ $orginator }{ $neighbour }{ "num_recv" };
+			$string .= " <> forw = " . ( $forward_hash{ $orginator }{ $neighbour }{ "num_forw" } ? $forward_hash{ $orginator }{ $neighbour }{ "num_forw" } : "0" );
+			$string .= " <> drop = " . ( $receive_hash{ $orginator }{ $neighbour }{ "num_drop" } ? $receive_hash{ $orginator }{ $neighbour }{ "num_drop" } : "0" );
+			$string .= " \t [ version = " . ( $receive_hash{ $orginator }{ $neighbour }{ "version" } ? $receive_hash{ $orginator }{ $neighbour }{ "version" } : "0" );
+			$string .= "; own_broad = " . ( $receive_hash{ $orginator }{ $neighbour }{ "own_broad" } ? $receive_hash{ $orginator }{ $neighbour }{ "own_broad" } : "0" );
+			$string .= "; own_rebroad = " . ( $receive_hash{ $orginator }{ $neighbour }{ "own_rebroad" } ? $receive_hash{ $orginator }{ $neighbour }{ "own_rebroad" } : "0" );
+			$string .= "; uni_flag = " . ( $receive_hash{ $orginator }{ $neighbour }{ "uni_flag" } ? $receive_hash{ $orginator }{ $neighbour }{ "uni_flag" } : "0" );
+			$string .= "; uni_link = " . ( $receive_hash{ $orginator }{ $neighbour }{ "uni_link" } ? $receive_hash{ $orginator }{ $neighbour }{ "uni_link" } : "0" );
+			$string .= "; dup = " . ( $receive_hash{ $orginator }{ $neighbour }{ "dup" } ? $receive_hash{ $orginator }{ $neighbour }{ "dup" } : "0" );
+			$string .= "; ttl = " . ( $receive_hash{ $orginator }{ $neighbour }{ "ttl" } ? $receive_hash{ $orginator }{ $neighbour }{ "ttl" } : "0" ) . " ]";
+			$string .= " [ direct_link = " . ( $forward_hash{ $orginator }{ $neighbour }{ "direct_link" } ? $forward_hash{ $orginator }{ $neighbour }{ "direct_link" } : "0" );
+			$string .= "; direct_uni = " . ( $forward_hash{ $orginator }{ $neighbour }{ "direct_uni" } ? $forward_hash{ $orginator }{ $neighbour }{ "direct_uni" } : "0" );
+			$string .= "; rebroad = " . ( $forward_hash{ $orginator }{ $neighbour }{ "rebroad" } ? $forward_hash{ $orginator }{ $neighbour }{ "rebroad" } : "0" );
+			$string .= "; dup = " . ( $forward_hash{ $orginator }{ $neighbour }{ "dup" } ? $forward_hash{ $orginator }{ $neighbour }{ "dup" } : "0" ) . " ]\n";
+
+		}
+
+		print "\norginator $orginator" . ( $myself_hash{ $orginator } ? " (myself)" : "" ) . ": total recv = $sum\n";
+		print $string;
+
+	}
+
+	print "\n\nHelp:\n^^^^\n";
+	print " Dropped packets:\n";
+	print "\tversion     = received packet indicated incompatible batman version\n";
+	print "\town_broad   = received my own broadcast\n";
+	print "\town_rebroad = received rebroadcast of my packet via neighbour\n";
+	print "\tuni_flag    = received packet with unidrectional flag\n";
+	print "\tuni_link    = received packet via unidirectional link\n";
+	print "\tdup         = received packet is a duplicate\n\n";
+	print "\tttl         = ttl of packet exceeded\n\n";
+
+	print " Forwarded packets:\n";
+	print "\tdirect_link = forwarded packet with direct_link flag\n";
+	print "\tdirect_uni  = forwarded packet with direct_link and unidirectional flag\n";
+	print "\trebroad     = just rebroadcast packet\n";
+	print "\tdup         = rebroadcast packet allthough it is a duplicate\n";
+
+} elsif ( $ARGV[0] eq "-s" ) {
+
+	foreach my $orginator ( keys %seq_hash ) {
+
+		print "\n\nOrginator: $orginator\n^^^^^^^^^\n";
+
+		foreach my $neighbour ( keys %{ $seq_hash{ $orginator } } ) {
+
+			print "\nNeighbour: $neighbour\n";
+
+			foreach my $seqno ( @{ $seq_hash{ $orginator }{ $neighbour } } ) {
+
+				print "$seqno ";
+
+			}
+
+			print "\n";
+
+		}
+
 	}
 
 }
 
 
-close(BATMANLOG);
 
+foreach my $todo ( keys %to_do_hash ) {
 
-print "\nSent:\n^^^^\n";
-
-foreach my $my_ip ( keys %myself_hash ) {
-
-	$total_seconds = ( $myself_hash{ $my_ip }{ "sent" } * $orig_interval ) / 1000;
-	print " => $my_ip (" . $myself_hash{ $my_ip }{ "if" } . "): send " . $myself_hash{ $my_ip }{ "sent" } . " packets in $total_seconds seconds\n";
+	print "ToDo: $todo -> $to_do_hash{ $todo }\n" if ( $to_do_hash{ $todo } > 2 );
 
 }
-
-
-print "\n\nReceived:\n^^^^^^^^";
-
-foreach my $orginator ( keys %receive_hash ) {
-
-	my $sum = 0;
-	my $string = "";
-
-	foreach my $neighbour ( keys %{ $receive_hash{ $orginator } } ) {
-
-		$sum += $receive_hash{ $orginator }{ $neighbour }{ "num_recv" };
-		$string .= " => $neighbour" . ( $myself_hash{ $neighbour } ? " (myself):\t" : ":\t\t" );
-		$string .= " recv = " . $receive_hash{ $orginator }{ $neighbour }{ "num_recv" };
-		$string .= " <> forw = " . ( $receive_hash{ $orginator }{ $neighbour }{ "num_forw" } ? $receive_hash{ $orginator }{ $neighbour }{ "num_forw" } : "0" );
-		$string .= " \t [ uni = " . ( $receive_hash{ $orginator }{ $neighbour }{ "uni" } ? $receive_hash{ $orginator }{ $neighbour }{ "uni" } : "0" );
-		$string .= "; bi = " . ( $receive_hash{ $orginator }{ $neighbour }{ "bi" } ? $receive_hash{ $orginator }{ $neighbour }{ "bi" } : "0" );
-		$string .= "; uni/bi = " . ( $receive_hash{ $orginator }{ $neighbour }{ "disagree" } ? $receive_hash{ $orginator }{ $neighbour }{ "disagree" } : "0" ) . " ]";
-		$string .= " [ not best = " . ( $receive_hash{ $orginator }{ $neighbour }{ "not_best" } ? $receive_hash{ $orginator }{ $neighbour }{ "not_best" } : "0" );
-		$string .= "; uni = " . ( $receive_hash{ $orginator }{ $neighbour }{ "uni" } ? $receive_hash{ $orginator }{ $neighbour }{ "uni" } : "0" );
-		$string .= "; incom = " . ( $receive_hash{ $orginator }{ $neighbour }{ "incom" } ? $receive_hash{ $orginator }{ $neighbour }{ "incom" } : "0" );
-		$string .= "; ttl = " . ( $receive_hash{ $orginator }{ $neighbour }{ "ttl" } ? $receive_hash{ $orginator }{ $neighbour }{ "ttl" } : "0" );
-		$string .= "; dup = " . ( $receive_hash{ $orginator }{ $neighbour }{ "dup" } ? $receive_hash{ $orginator }{ $neighbour }{ "dup" } : "0" ) . " ]\n";
-
-	}
-
-	print "\norginator $orginator" . ( $myself_hash{ $orginator } ? " (myself)" : "" ) . ": total recv = $sum\n";
-	print $string;
-
-}
-
-print "\n\nHelp:\n^^^^\n";
-print "\tuni      = received packet with unidirectional flag (won't be forwarded)\n";
-print "\tbi       = received packet via bidirectional link\n";
-print "\tuni/bi   = neighbour thinks connection is bidirectional - I disagree\n";
-print "\tnot best = received packet didn't came via my best neighbor (won't be forwarded)\n";
-print "\tincom    = received packet indicated incompatible batman version (will be ignored)\n";
-print "\tttl      = ttl of packet exceeded (won't be forwarded)\n";
-print "\tdup      = received packet is a duplicate\n";
