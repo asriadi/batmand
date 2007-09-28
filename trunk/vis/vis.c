3ea1a48db1e39f42ca28c40cf68ed3d252312f16
marek <marek@45894c77-fb22-0410-b583-ff6e7d5dbf6c>
2007-09-28 19:02:01 +0000

    fix vis server version string

--- trunk/vis/vis.c
+++ trunk/vis/vis.c
@@ -610,7 +610,7 @@ void *master() {
 
 void print_usage() {
 
-	printf( "B.A.T.M.A.N. visualisation server %s\n", VERSION );
+	printf( "B.A.T.M.A.N. visualisation server %s\n", SOURCE_VERSION );
 	printf( "Usage: vis <interface(s)> \n" );
 	printf( "\t-d debug level\n" );
 	printf( "\t-h help\n" );
@@ -665,7 +665,7 @@ int main( int argc, char **argv ) {
 				break;
 
 			case 'v':
-				printf( "B.A.T.M.A.N. visualisation server %s\n", VERSION );
+				printf( "B.A.T.M.A.N. visualisation server %s\n", SOURCE_VERSION );
 				exit(EXIT_SUCCESS);
 				break;
 
@@ -772,7 +772,7 @@ int main( int argc, char **argv ) {
 	}
 
 
-	debug_output( "B.A.T.M.A.N. visualisation server %s successfully started ... \n", VERSION );
+	debug_output( "B.A.T.M.A.N. visualisation server %s successfully started ... \n", SOURCE_VERSION );
 
 
 	pthread_create( &udp_server_thread, NULL, &udp_server, NULL );
