5a09deec4fb84b0d9a0b310be47d78a1eda31e3b
simon <simon@45894c77-fb22-0410-b583-ff6e7d5dbf6c>
2009-01-25 14:26:25 +0000

    update version string to 0.3.2-beta

--- vis/vis.h
+++ vis/vis.h
@@ -36,7 +36,7 @@
 #include "allocate.h"
 #include "list-batman.h"
 
-#define SOURCE_VERSION "0.3.1-beta" //put exactly one distinct word inside the string like "0.3-pre-alpha" or "0.3-rc1" or "0.3"
+#define SOURCE_VERSION "0.3.2-beta" //put exactly one distinct word inside the string like "0.3-pre-alpha" or "0.3-rc1" or "0.3"
 
 
 #define MAXCHAR 4096
